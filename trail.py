859e7ef8ea7a0df9ca3e92d4992ebca61981a04a
Anshul <anshulsoni2003@gmail.com>
2023-04-12 14:34:48 +0530

    fixed trail.py for transaction flow

--- trail.py
+++ trail.py
@@ -7,7 +7,8 @@ from datetime import datetime, timedelta
 
 # create an empty graph
 G = nx.Graph()
-
+graph = []
+date_format = '%Y-%m-%d %H:%M:%S'
 # add nodes
 for i in range(1, 81):
     G.add_node(i)
@@ -20,7 +21,10 @@ with open("dummy_data.csv", "r") as f:
         from_acc = int(row[1])
         to_acc = int(row[2])
         amount = float(row[3])
+        date = str(row[4])
+        graph.append([from_acc, to_acc, amount, date])
         G.add_edge(from_acc, to_acc, amount=amount)
+    print(graph)
 
 # parse command-line arguments
 parser = argparse.ArgumentParser(description='AML search tool')
@@ -28,30 +32,36 @@ parser.add_argument('from_acc', type=int, help='the account to start from')
 parser.add_argument('-m', action='store_true', help='account with max transactions from the given account')
 parser.add_argument('-amount', type=int, help='transactions above this amount')
 parser.add_argument('-t', choices=['day', 'week', 'month'], default='month', help='transactions within the specified time period (default: %(default)s)')
+parser.add_argument('-date', type=str,help ='the date of transaction')
 args = parser.parse_args()
 
 
 # filter edges based on command-line arguments
 edges = []
-for from_acc, to_acc, attributes in G.edges(data=True):
-    amount = attributes['amount']
+for i in graph:
+    from_acc = i[0]
+    to_acc = i[1]
+    amount = i[2]
+    date = i[3]
     if from_acc == args.from_acc:
         if args.amount is None or amount >= args.amount:
             if args.t == 'day':
-                date_format = '%Y-%m-%d %H:%M:%S'
-                txn_date = datetime.strptime('2023-04-12 00:00:00', date_format) - timedelta(days=1)
+                # date_format = '%Y-%m-%d %H:%M:%S'
+                txn_date = datetime.strptime(args.date, date_format) - timedelta(days=1)
                 txn_date_str = txn_date.strftime(date_format)
-                if attributes.get('date', '') >= txn_date_str:
-                    edges.append((from_acc, to_acc, amount))
+                print(txn_date_str)
+                if date >= txn_date_str:
+                # if attributes.get('date', '') >= txn_date_str:
+                    edges.append((from_acc, to_acc, amount, date))
             elif args.t == 'week':
-                date_format = '%Y-%m-%d %H:%M:%S'
-                txn_date = datetime.strptime('2023-04-12 00:00:00', date_format) - timedelta(weeks=1)
+                # date_format = '%Y-%m-%d %H:%M:%S'
+                txn_date = datetime.strptime(args.date, date_format) - timedelta(weeks=1)
                 txn_date_str = txn_date.strftime(date_format)
-                if attributes.get('date', '') >= txn_date_str:
-                    edges.append((from_acc, to_acc, amount))
+                if date >= txn_date_str:
+                    edges.append((from_acc, to_acc, amount, date))
             else:
-                edges.append((from_acc, to_acc, amount))
-
+                edges.append((from_acc, to_acc, amount, date))
+                
 
 # sort edges by transaction amount
 if len(edges) > 0:
@@ -63,14 +73,14 @@ if len(edges) > 0:
     for edge in edges:
         to_acc = edge[1]
         amount = edge[2]
-        date = edge[2].get('date', '')
+        date = edge[3]
         print(f"{args.from_acc} -> {to_acc}: ${amount:,} ({date})")
 else:
     print(f"No transactions found for account {args.from_acc}")
 
 # create a subgraph based on the filtered edges
 print(edges)
-l = [(x, y, d) for x, y, d in edges]
+l = [(x, y, d, w) for x, y, d, w in edges]
 print(l)
 subgraph = G.edge_subgraph(l)
 # if requested, find the account with the max transactions from the given account
@@ -88,6 +98,6 @@ if args.m:
 # draw the subgraph
 pos = nx.spring_layout(subgraph)
 nx.draw(subgraph, pos, with_labels=True, font_weight='bold', node_size=500, font_size=10)
-edge_labels = {(u, v): d for u, v, d in subgraph.edges(data=True)}
+edge_labels = {(u, v): d for u, v, d, w in subgraph.edges(data=True)}
 nx.draw_networkx_edge_labels(subgraph, pos, edge_labels=edge_labels, font_size=8)
 plt.show()
